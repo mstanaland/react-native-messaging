42e1c1ea32b2a870416805775a490e6655862828
Michael Stanaland <mstanaland@mac.com>
2018-04-10 06:25:34 -0400

    Initial commit

---
+++ App.js
@@ -0,0 +1,170 @@
+import React from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  Alert,
+  Image,
+  TouchableHighlight,
+  BackHandler
+} from "react-native";
+
+import Status from "./components/Status";
+import MessageList from "./components/MessageList";
+import {
+  createImageMessage,
+  createLocationMessage,
+  createTextMessage
+} from "./utils/MessageUtils";
+
+export default class App extends React.Component {
+  state = {
+    messages: [
+      createImageMessage("https://unsplash.it/300/300"),
+      createTextMessage("World"),
+      createTextMessage("Hello"),
+      createLocationMessage({
+        latitude: 37.78825,
+        longitude: -122.4324
+      })
+    ],
+    fullscreenImageId: null
+  };
+
+  componentWillMount() {
+    // Android only - handles hardware back button so it dismisses the fullscreen
+    // image.
+    this.subscription = BackHandler.addEventListener('hardwareBackPress', () => {
+      const { fullscreenImageId } = this.state;
+
+      if (fullscreenImageId) {
+        this.dismissFullScreenImage();
+        // Return true to prevent exiting from app
+        return true;
+      }
+      // Return false - back button will exit the app (default behavior)
+      return false;
+    })
+  }
+
+  componentWillUnmount() {
+    this.subscription.remove();
+  }
+
+  dismissFullScreenImage = () => {
+    this.setState({ fullscreenImageId: null });
+  };
+
+  handlePressMessage = ({ id, type }) => {
+    switch (type) {
+      case "text":
+        Alert.alert(
+          "Delete message?",
+          "Are you sure you want to permanently delete this message?",
+          [
+            {
+              text: "Cancel",
+              style: "cancel"
+            },
+            {
+              text: "Delete",
+              style: "destructive",
+              onPress: () => {
+                const { messages } = this.state;
+                this.setState({
+                  messages: messages.filter((message) => message.id !== id)
+                });
+              }
+            }
+          ]
+        );
+        break;
+      case "image":
+        this.setState({ fullscreenImageId: id });
+        break;
+      default:
+        break;
+    }
+  };
+
+  renderMessageList() {
+    const { messages } = this.state;
+
+    return (
+      <View style={styles.content}>
+        <MessageList
+          messages={messages}
+          onPressMessage={this.handlePressMessage}
+        />
+      </View>
+    );
+  }
+
+  renderInputMethodEditor() {
+    return <View style={styles.inputMethodEditor} />;
+  }
+
+  renderToolBar() {
+    return <View style={styles.toolbar} />;
+  }
+
+  renderFullscreenImage = () => {
+    const { messages, fullscreenImageId } = this.state;
+
+    if (!fullscreenImageId) return null;
+
+    const image = messages.find((message) => message.id === fullscreenImageId);
+
+    if (!image) return null;
+
+    return (
+      <TouchableHighlight
+        style={styles.fullscreenOverlay}
+        onPress={this.dismissFullScreenImage}
+      >
+        <Image style={styles.fullscreenImage} source={{ uri: image.uri }} />
+      </TouchableHighlight>
+    );
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Status />
+        {this.renderMessageList()}
+        {this.renderToolBar()}
+        {this.renderInputMethodEditor()}
+        {this.renderFullscreenImage()}
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff"
+  },
+  content: {
+    flex: 1,
+    backgroundColor: "white"
+  },
+  inputMethodEditor: {
+    flex: 1,
+    backgroundColor: "white"
+  },
+  toolbar: {
+    borderTopWidth: 1,
+    borderTopColor: "rgba(0,0,0,0.04)",
+    backgroundColor: "white"
+  },
+  fullscreenOverlay: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: "black",
+    zIndex: 2
+  },
+  fullscreenImage: {
+    flex: 1,
+    resizeMode: "contain"
+  }
+});
